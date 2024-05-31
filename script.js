d386738900a1db9bf2d232d8bf901a7e06e50ec8
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-05-31 11:18:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+
 // Your code here.
+const itemsContainer = document.querySelector(".items");
+const items = document.querySelectorAll(".item");
+
+let isDragging = false;
+let startX;
+let scrollLeft;
+
+itemsContainer.addEventListener("mousedown", (e) => {
+  isDragging = true;
+  startX = e.pageX - itemsContainer.offsetLeft;
+  scrollLeft = itemsContainer.scrollLeft;
+  itemsContainer.classList.add("active");
+});
+
+itemsContainer.addEventListener("mouseup", () => {
+  isDragging = false;
+  itemsContainer.classList.remove("active");
+});
+
+itemsContainer.addEventListener("mouseleave", () => {
+  isDragging = false;
+  itemsContainer.classList.remove("active");
+});
+
+itemsContainer.addEventListener("mousemove", (e) => {
+  if (!isDragging) return;
+  e.preventDefault();
+  const x = e.pageX - itemsContainer.offsetLeft;
+  const walk = (x - startX) * 2;
+  itemsContainer.scrollLeft = scrollLeft - walk;
+});
+
+itemsContainer.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+});
